79cc58037dda51220f046398a1005c6f96950110
ShilovVlad <v.shilov.mobile@gmail.com>
2021-01-09 18:26:58 +0300

    VSh 20210109

--- HellowWorld/src/main/kotlin/ru/vladshilov/lesson1/Main.kt
+++ HellowWorld/src/main/kotlin/ru/vladshilov/lesson1/Main.kt
@@ -4,9 +4,11 @@ import java.time.LocalDate
 
 fun main(){
     // fun will
-    val cntr = Contract("233", LocalDate.of(2020,12,31), 258000.00, "OOO Powerbank")
+    val cntr1 = Contract("233", LocalDate.of(2020,12,31), 258000.00, "OOO Powerbank")
+    val cntr2 = Contract("156", LocalDate.of(2020,12,20), 325012.00, "OOO Portal")
 
 
-    println(cntr)
+    println(cntr1)
+    println(cntr2)
 
 }
